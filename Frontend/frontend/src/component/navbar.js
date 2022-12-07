f418f855b0964f799e12390bc2c04ae95284d0ee
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2022-12-07 11:45:38 +0700

    add navbar

---
+++ Frontend/frontend/src/component/navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "./navbar.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div class="Navbar" style={{ zIndex: '9999' }}>
+            <div id="bigborder">
+                <div class="sidebar">
+                    <div id="logo">LOGO</div>
+
+                    <div id="profile_group">
+                        <i class="bi bi-person-circle"></i>
+                        <div id="username">Đặng Việt</div>
+                        <div id="role">ADMIN</div>
+                    </div>
+
+                    <button type="button" autoFocus>
+                        <a href="/"> <i class="bi bi-house-door"></i> Dashboard</a>
+                    </button>
+
+                    <button type="button">
+                        <a href="#"> <i class="bi bi-person"></i> Users</a>
+                    </button>
+
+                    <button type="button">
+                        <a href="#"> <i class="bi bi-person-vcard"></i> Information</a>
+                    </button>
+
+                    <button type="button">
+                        <a href="#"> <i class="bi bi-clipboard-data"></i> Accounting</a>
+                    </button>
+
+                    <button type="button">
+                        <a href="#"> <i class="bi bi-book"></i> Classrooms</a>
+                    </button>
+
+                    <button type="button">
+                        <a href="#"> <i class="bi bi-calendar-check"></i> Attendance</a>
+                    </button>
+
+                    <button type="button">
+                        <a href="#"> <i class="bi bi-calendar3"></i> Schedules</a>
+                    </button>
+
+                    <button type="button">
+                        <a href="#"> <i class="bi bi-clipboard2-check"></i> Noticeboard</a>
+                    </button>
+
+                    <button type="button">
+                        <a href="#"> <i class="bi bi-bell"></i> Notifications</a>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
